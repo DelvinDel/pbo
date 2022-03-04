3d4ce49eb03192d6d73c8a8624965ed64c541e91
Delvin <97678699+DelvinDel@users.noreply.github.com>
2022-03-04 15:25:10 +0700

    02

---
+++ 04/ProblemSet4_1/UsingMethods/src/com/example/ComputeMethods.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import java.util.Random;
+
+
+public class ComputeMethods {
+    public double fToC(double degreeF){
+
+double Fahrenheit = 90;
+
+double Celcius = 5 / 9 * (Fahrenheit - 32);
+        System.out.println("Temp in celcius is ");
+return Celcius;
+}
+    
+}
+
