ea40193718a77f13b4498db6b25f80df3f644968
Delvin <97678699+DelvinDel@users.noreply.github.com>
2022-03-04 15:38:10 +0700

    02

--- 02/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
+++ 02/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -3,8 +3,15 @@ package problemset2_2;
 public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
-        //Use comments to describe what your image is depicting.
-        
+        //A jar with Infinity symbol
+        System.out.println("  ____  ");
+        System.out.println(" /    \\ ");
+        System.out.println("/  OO  \\");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("\\      /");
+        System.out.println(" \\    / ");
+        System.out.println("  ----");
         
         
         
