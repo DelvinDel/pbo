563cc60fcdd9ec72b9c62394efe6dba516954c53
Delvin <97678699+DelvinDel@users.noreply.github.com>
2022-03-22 16:39:07 +0700

    06-01

---
+++ UTS/src/uts/UTS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+
+public class UTS {
+
+
+    public static void main(String[] args) {
+short a=1,b=2,c;
+c =  (a+b);
+        System.out.println(c);
+    }
+    
+}
