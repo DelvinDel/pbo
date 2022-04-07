771da40676737e142a5cd3f54a88f73481fa6ff1
Delvin <97678699+DelvinDel@users.noreply.github.com>
2022-04-07 15:32:09 +0700

    06-problem set

--- prisionerTest/src/prisionertest/PrisionerTest.java
+++ prisionerTest/src/prisionertest/PrisionerTest.java
@@ -1,22 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prisionertest;
 
-/**
- *
- * @author pisco
- */
+
 public class PrisionerTest {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        prisioner buba = new prisioner();
-        prisioner twich = new prisioner();
-        System.out.println(buba == twich);
+        prisioner bubba = new prisioner();
+        prisioner twitch = new prisioner();
+        
+    bubba.nama = "bubba";
+    twitch.nama = "bubba";
+        System.out.println(bubba.nama == twitch.nama);
     }
     
 }
